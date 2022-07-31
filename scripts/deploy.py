1b30ae16f73c9c3a717737e57a23f7ef0070c08a
Ayush Bansal <ayushb6732@gmail.com>
2022-07-31 13:55:14 +0530

    Basics

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,21 +2,18 @@ from brownie import LandRegistry, accounts, config, network
 from scripts.helpful_scripts import get_account
 from web3 import Web3
 
-executorAddressPKey = config["wallets"]["executor_addr"]
-executorAddress = config["wallets"]["executor_addr_pub_key"]
+ownerAddress = config["wallets"]["owner_addr"]
+executorAddress = config["wallets"]["executor_addr"]
 sellerAddress = config["wallets"]["seller_addr"]
 buyerAddress = config["wallets"]["buyer_addr"]
 
+
 def main():
-    # if LandRegistry == []:
-    #     land_registry = LandRegistry.deploy({"from": get_account()})
-    # else:
-    #     land_registry = LandRegistry[-1]
-    land_registry = LandRegistry.deploy({"from": get_account()})
+    # LandRegistry.deploy(
+    #     {"from": get_account(ownerAddress)})
+    if LandRegistry == []:
+        land_registry = LandRegistry.deploy(
+            {"from": get_account(ownerAddress)})
+    else:
+        land_registry = LandRegistry[-1]
     print(land_registry)
-
-
-# Set up brownie networks by command -
-# brownie networks add polygon polygon-alchemy host=<Alchemy APP HTTP Address> chainid=80001
-# then run the script using command -
-# brownie run scripts/deploy.py --network polygon-alchemy
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -4,15 +4,5 @@ LOCAL_BLOCKCHAIN_ENVIRONMENT = ["development", "ganache-local"]
 FORKED_LOCAL_ENVIRONMENT = ["mainnet-fork", "mainnet-fork-dev"]
 
 
-def get_account(index=None, id=None):
-
-    if index:
-        return accounts[index]
-
-    if id:
-        return accounts.load(id)
-
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENT or network.show_active() in FORKED_LOCAL_ENVIRONMENT:
-        return accounts[0]
-    else:
-        return accounts.add(config["wallets"]["from_key"])
+def get_account(privateKey):
+    return accounts.add(privateKey)
