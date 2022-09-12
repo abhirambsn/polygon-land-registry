f393de8ab673fa7ed76d6cb39933315f62407f44
Abhiram B.S.N <abhirambsn@gmail.com>
2022-09-12 19:52:43 +0530

    Contract Redeployed

--- front_end/lib/constants.js
+++ front_end/lib/constants.js
@@ -21,5 +21,5 @@ export const customModalStyles = {
 export const formatAddress = (addr) =>
   addr ? addr.slice(0, 5) + "..." + addr.slice(-5) : null;
 
-export const CONTRACT_ADDRESS = "0x0AafE4EAF95aAaF0C19Eb223432F6b780eA99059";
+export const CONTRACT_ADDRESS = "0xc0F1b92Aa7Bcc79164F13eBf5DD36B65bd598efe";
 export const CONTRACT_ABI = landRegistryAbi.abi;
